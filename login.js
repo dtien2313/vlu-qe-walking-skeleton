f290f2ef958f5584e6be066fc2d78c1e0957b887
dtien2313 <ductien125048@gmail.com>
2026-09-17 18:53:38 +0700

    Create login.js

---
+++ login.js
@@ -0,0 +1,11 @@
+function login(username, password) {
+    if (username === 'admin' && password === '123') {
+        return true;
+    }
+
+    return false;
+}
+
+if (typeof module !== 'undefined') {
+    module.exports = login;
+}
